eb5ff4b2dba4e3e7433edc7758e617c26dc60a50
goztrk <me@gokhan.org.tr>
2023-07-24 18:49:25 +0300

    Add version flag

--- commands.js
+++ commands.js
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
 
 const program = new Command();
-program.name('new-blog-post').description('Creates a new markdown file for blog post');
+program.name('new-blog-post').description('Creates a new markdown file for blog post').version('0.0.1', '-v');
 
 program.parse();
